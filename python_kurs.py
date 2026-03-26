ce651b4ceab938638b21c8044999a9ca432af9d3
azizislomoff <azizislomoff@gmail.com>
2026-03-27 01:34:43 +0500

    yangi fayl
    1-faylim

---
+++ python_kurs.py
@@ -0,0 +1,601 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 14 17:32:26 2026
+
+@author: acer
+"""
+
+#print("hello world")
+#print('assalomu alaykum')
+#print("hello world")
+#print("salom meni ismim Aziz")
+#print("yoshim 23 da")
+#print('yo\'q g\'oya o\'rin ko\'ylak')
+#print("Men Yangi \"noutbook\" oldim")
+#print("""men ertaga bozorga boraman
+#va yangi so'mka olaman""")
+#print("""men dars
+#qilayabman""")
+#print('Men bugun ichtimoiy \ntarmoqlarda kop vaqt bolmadim \nsababi dars qildim va o\'qidim')
+#print("salom meni \nismim \"Aziz\" men \n23 yoshdaman va \"hozirda\" dasturlashni \nurganayabman")
+#print(5+9/3)
+#print("10"+"5")
+#print(59-21+69*62/23)
+#print(5**2)
+#print(2**10)
+#print(8/2)
+#print(5/2)
+#print(7//2)
+#print("Ikkini kvadrati", 2**2, "ga teng")
+#print("to'rtning kvadrati", 4**2, "ga teng") 
+#print("3*3=",3*3)
+#ism = "Aziz"
+#yosh = 23
+#sharf = "Islomov"
+#print(ism)
+#print(yosh)
+#print(sharf)
+#a = 5
+#b = 8
+#c = ((a+b)**2)
+#print(c)
+#radus=5
+#pi=3.14
+#print()
+#aylana_yuzi =pi*radus**2
+#print("Radusi", radus, "ga teng aylanani yuzi", aylana_yuzi,)
+#ism="Aziz"
+#print(ism)
+#ism="sardor"
+#print(ism)
+#print(f"Men {ism} yani {ism_sharf} {viloyat} viloyati {tuman} tumanida tug\'ilganman yani {tugilgan_joy} yoshim {yosh} yoshda {yil} yildaman yani {yosh_yil}" )
+#print(f'ikki kara ikki {2**2}')
+#print('Hello world')
+#print("Hello \tworld")
+#print('Hello \nworld')
+#ism = "aziz"
+#familiya = "islomov"
+#ism_sharf = f'{ism} {familiya}'
+#ism_sharf = ism_sharf.upper()
+#print(ism_sharf.upper())
+#print(ism_sharf.lower())
+#print(ism_sharf.title())
+#print(ism_sharf.capitalize())
+# = "     xurmo     "
+#print(meva)
+#print('men', meva, 'yaxshi ko\'raman')
+#print('men', meva.lstrip(), 'yaxshi ko\'raman')
+#print('men', meva.rstrip(), 'yaxshi ko\'raman')
+#print('men', meva.strip(), 'yaxshi ko\'raman')
+#ism = input("Ismingiz nima\n>>>")
+#print('Assalomu alaykum ' f'{ism.title()}')
+#print("Assalumu alaykum " + ism.title())
+#kucha = "bog\'bon"
+#mahalla = "sag\'bon"
+#tuman = "bodomzor"
+#viloyat ="samarqand"
+#print(kocha,  'kuchasi,',  mahalla,  'maahallasi,', tuman,  'tumani,',  viloyat,  'viloyati.')
+#kucha = input('kuchangiz?')
+#mahalla = input('mahallangiz')
+#tuman = input('tumaningiz')
+#viloyat = input('viloyatingiz')
+#print(kucha.upper() + ' kuchasi\n ' +  mahalla.upper() + ' mahallasi\n ' + tuman.upper() + ' tumani\n ' +  viloyat.upper() + ' viloyati\n ' )
+#manzil = f'{kucha} kuchasi, {mahalla} mahallasi {tuman} tumani {viloyat} viloyati'
+#print(manzil)
+#print(manzil.upper())
+#print(manzil.lower())
+#print(manzil.title())
+#print(manzil.capitalize())
+#ism = "      Aziz      "
+#print(' meni ismim ' + ism)
+#print(' meni ismim ' + ism.lstrip())
+#print(' meni ismim ' + ism.rstrip())
+#print(' meni ismim ' + ism.strip())
+#a = int(input("Birinchimraqam"))
+#b = int(input("ikkinchi raqam"))
+#c = a+b
+#print(c)
+#ism = "Aziz"
+#yosh = 23
+#nechi = ism + ' ' + str(yosh) + " yoshda"
+#print(nechi)
+#PI = 3.1456
+#ism = input("Ismingiz nima? ")
+#yosh = input("Yoshingiz nechida? ")
+#print(f'Assalomu alaykum {ism.upper()} yoshingiz {yosh} da.')
+#   RO'YXAT BILAB ISHLASH 
+#yosh = int(input('Yoshingiz nechida \n>>>'))
+#yil = 2026-yosh
+#print('siz', yil, "tug'ilgan ekansiz")
+
+# son_1 = input("Birinchi sonni kiriting >>> ")
+# son_2 = input("Ikkinchi sonni kiriting >>> ")
+# a = int(son_1) + int(son_2)
+# b = int(son_1) - int(son_2)
+# c = int(son_1) * int(son_2)
+# d = int(son_1) / int(son_2)
+# print(son_1, '+', son_2, "=", a)
+# print(son_1, '-', son_2, "=", b)
+# print(son_1, '*', son_2, "=", c)
+# print(son_1, '/', son_2, "=", d)
+# mevalar = ['olma', 'gilos', 'anor', 'shaftoli', 'uzum']
+# narxlar = [1200, 5500, 65200, 1230, 2200, 32510]
+# mevalar.append('tarvuz')
+# gullar = []
+# mevalar.insert(0, 'anjir')
+# hayvonlar = ['it', 'mushuk', 'echki', "it" ]
+# bozorlik = ["yog'", 'un', 'piyoz', 'banan', "go'sht", "sabzi"]
+# print("Men", bozorlik.pop(4), 'sotib oldim')
+# print("Qolgan maxsulotlar", bozorlik)
+      # VAZIFA
+# ismlar = ['aziz', 'sardor', 'fazliddin', "ahad", "salim"]
+# print(f"Salom {ismlar[0].title()} ahvollar qalay")
+# print(ismlar[1].upper(), 'Jizzaxda ishlaydi')
+# print('Navoiydan' + ' ' + ismlar[2].title() + " " + "kelayabdi ")
+ 
+# sonlar = [12, 100, -200, 36.5, 20, -52, 60, 5.6]
+# print(sonlar[0] + sonlar[3])
+  
+# t_shaxslar = ["Al Buhoriy", "Amir Temur", 'Mirzu Ulug\'bek']
+# z_shaxslar = ['Ranaldo', 'Bil geds', 'SHavkat Mirziyoyiv']
+# print("Men tarixiy shaxslardan", t_shaxslar.pop(1).upper(), "\n\
+# # bilan, zamonaviy shaxslardan esa", z_shaxslar.pop(-1), "bilan suhbat qilishni istar edim")
+ 
+# frinds = ["jonibek", 'mashrab', 'sayid', 'sardor', 'nurbek']
+# mehmonlar = []
+# mehmonlar.append(frinds.pop(0))
+# mehmonlar.append(frinds.pop(-1))
+# mehmonlar.append(frinds.pop(2))
+
+# a = input("Salam ismingiz nima \n>>> ")
+# b = int(input("Yoshingiz nechida \n>>> "))
+# c = 2025-b
+# print(f"Assalomu alaykum {a.title()} sizning tug'ilgan yilingiz {c}")
+
+# ismlar = ['javohir', 'odil', 'begzod', "aziz", "temur", "salim", 'nurbek']
+# ismlar.title()
+
+# print(f"Salom bolalar bugun darsda {ismlar.sort()}")
+# sonlar = list(range(0,11))
+# d = [100, 233, 445, 456465, 6565, 555151, 55555, 8, 85, 34, 2, 0, 3, 4, 2635522, 552114, 552]
+
+#sonlar = list(range(0,10))
+# juft_sonlar = list(range(0,11,2))
+# toq_sonlar = list(range(1,10,2))
+# print(juft_sonlar)
+# print(toq_sonlar)
+
+# ismlar = ("aziz", 'azamat', 'sardor', "fazliddin")
+# ismlar = list(ismlar)
+# ismlar.append("vali")
+#davlatlar = ["Uzbekiston", "Rossiya", "Amerika", 'Germaniya', 'Fransiya', "Turkiya"]
+#print("Elementlar uzunligi", len(davlatlar)                                                         
+#print(sorted(davlatlar, reverse = True))
+
+# davlatlar = ["Uzbekiston", "Rossiya", "Amerika", 'Germaniya', 'Fransiya', "Turkiya"]
+# print(sorted(davlatlar, reverse = True))
+
+# mevalar = ['olma', 'gilos', 'o\'rik', 'anor', 'banan', 'shaftoli', "uzum"]
+# mevalar.sort()
+
+# juft_sonlar = list(range(120,1200,2))
+# print(juft_sonlar)
+# print(max(juft_sonlar) - min(juft_sonlar))
+# print(juft_sonlar)
+# print(juft_sonlar[:20])
+# print(juft_sonlar[-20:])
+# print(juft_sonlar[520:540])
+
+# taomlar = ["Osh", "Manti", "Sho'rva", "Mastava", "Lag'mon"]
+# nonushta = taomlar[:]
+# nonushta = tuple(nonushta)
+
+# mehmonlar = ['Aziz', 'Vali', 'Ali', 'Jamol', "Karim"]
+# for mehmon in mehmonlar:
+#    print("salom", mehmon)
+   
+# print('Kod', len(mehmonlar), "marta takrorlandi")
+
+# toq_sonlar = list(range(11,100,2))
+# for toq_son in toq_sonlar:
+#    print(f"{toq_son} kubi {toq_son**3}")
+
+# kinolar = []
+# print("5 ta eng sevimli kinoyingiz")
+# for kino in range(6): 
+#     kinolar.append(input(f"{kino+1} chi kino \n>>> "))
+# print(kinolar)
+
+# mevalar =[]
+# print("5 ta eng sevimli me'vam")
+# for n in range(5):
+#    mevalar.append(input(f"{n+1} chi meva >>>  "))
+# print(mevalar)
+# suhbatdoshlar = []
+# a = input("Bugun necha kishi bilan suhbat qurding >>>  ")
+# for suhbat in range(int(a)):
+#   suhbatdoshlar.append(input(f"{suhbat+1} chi inson kim >>> "))
+# print(len(suhbatdoshlar), "ta inson bilan suhbatlashdingiz")
+# print(suhbatdoshlar)
+
+#sonlar = list(range(1,11))
+#juft_sonlar = list(range(2,11,2))
+
+#mevalar = ['olma', 'anor', 'shaftoli', 'gilos']
+#for meva in mevalar:
+#  print(meva.upper())
+
+#sonlar = list(range(1,11))
+#print(sum(sonlar))    
+
+# ismlar = ['ali', "vali", 'hasan', "husan"]
+# for ism in ismlar:
+#   print(f'Salom {ism.title()}')
+
+# cars = ['toyota', 'mazda', 'hyudai', "gm", "kia"]
+# for car in cars:
+#    if car == "gm" or car == "kia":
+#      print(car.upper())
+#    else:
+#      print(car.title())
+
+# gullar =["lola", "atirgul", "binafsha gul", "kaktus", "geran"]
+# for gul in gullar:
+#   if gul != "kaktus":
+#     print(gul.upper())
+#   else:
+#     print(gul.title())
+
+# foydalanuvchi_ismi = input("Sizning logeningiz nima >>> ")
+# foydalanuvchi_ismi = foydalanuvchi_ismi.lower()
+# if foydalanuvchi_ismi == "admin":
+#   print("xush kelibsiz, Admin. \nFoydalanuvchilar ro'yxatini ko'rasizmi")
+# else:
+#   print(f"Xush kelibsiz {foydalanuvchi_ismi.title()}")
+
+# print("2 ta son kiriting")
+# a = int(input("1 chi son >>> "))
+# b = int(input("2 chi son >>> "))
+# if a==b:
+#   print("Sonlar teng")
+# else:
+#   print("Sonlar teng emas")
+
+# a = input("Istalgan son yozing >>> ")
+# print("Manfiy son") if int(a)<0 else print("Musbat son")
+  
+# a = input("Istalgan son yozing >>> ")
+# if int(a)<0: 
+#   print("Manfiy son")
+# else:
+#   print("Musbat son")
+
+#son = float(input("Istalgan son kiritng"))
+#print(son**0.5) if son>0  else print("Manfiy son")
+
+# ismlar = ["aziz", "fazliddin", "ali", "vali", 'hasan']
+# for ism in ismlar:
+#  if ism == "aziz":   
+#     print("Salom", ism.title())
+#  else:
+#     print(ism.upper())
+
+# a = input("Birinchi sonni yoz >>> ")
+# b = input("Ikkinchi sonni yoz >>>  ")
+# print(int(a)+int(b))
+
+# sonlar = list(range(1,11,))
+# for son in sonlar:
+#   if son % 2==0:
+#    print("Juft son")
+#   else:
+#    print('toq son')
+
+# sonlar = list(range(1,21))
+# for son in sonlar:
+#   if son%3==0:
+#    print("Fizz")
+#   else:
+#    print(son)
+
+# sonlar = [3,7,10,15,8,21]
+# for son in sonlar:
+#   if son>=10:
+#    print(f'{son} 10 dan katta')
+#   else:
+#    print(f'{son} 10 dan kichkina')
+
+# sonlar = list(range(1,16))
+# for son in sonlar:
+#   if son%5==0:
+#     print(son, "Beshga bo'linadi")
+#   else:
+#     print(son, "Bo'linmaydi")
+
+# a = 50
+# print("Musbat")  if a>0 else print('Manfiy')
+
+# mevalar = ['olma', 'uzum', "gilos", "anor", "banan"]
+# for meva in mevalar:
+#  if meva == "olma":
+#   print(meva.upper(), "daraxti")
+#  else:
+#   print(meva.title(), "daraxti")
+
+# meva =[]
+# print("Siz yoqtirgan mevalar")
+# for v in range(8):
+#    meva.append(input(f"{v+1} chi meva >>> "))
+# print(meva)
+
+# yil = int(input("Tug'ilgan yilingiz nechi >>>  "))
+# if 2026-yil >= 18:
+#   a = input("ismingiz nima >>> ")  
+#   print("salom ", a)
+# else:
+#   print("siz yosh bolasiz")
+
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#  if car != 'gm':
+#   print(car.upper())
+#  else:
+#   print(car.title())
+
+# ism = input("Logen ismingiz nima >>>  ")
+# if ism.lower() == "admin":
+#   print("Xush kelibsiz, Admin. Foydalanuvchilar ro'yxatini ko'rasizmi?")
+# else:
+#   print(f"Xush kelibsiz, {ism}!")
+
+#    11 DARS UCHUN TOPSHIRIQLAR
+# son = float(input("Juft son kiriting >>>  "))
+# if son%2==0:
+#   print("Raxmat.")
+# else:
+#   print("Juft son kiriting.")
+
+# yosh = int(input("Yoshingiz nechida >>> "))
+# if yosh<=4 :
+#  narx = 0
+# elif yosh<=18: 
+#   narx = 10000
+# elif yosh<60:
+#  narx = 30000
+# else:
+#  narx = 0
+# print(f"sizga kirish {narx} so'm")
+
+# son1 = int(input("Birinchi sonni kiriting >>>  "))
+# son2 = int(input("Ikkinchi sonni kiriting >>>  "))
+# if son1>son2:
+#  print(f"{son1}>{son2}")
+# elif son1<son2:
+#  print(f"{son1}<{son2}")
+# else:
+#  print(f"{son1}={son2}")
+
+# mahsulotlar = ['Sabzi', "Uzum", "Shkalad", "Apilsen", 'Olma',
+#                "Tarvuz","Coco cola", "Piyoz", "Bodring", "Qalampir"] 
+# savat =[]
+# print("Savatga kamida 5 ta mahsulot kiriting")
+# for n in range(5):
+#   savat.append(input(f'{n+1} chi  masulotingiz >>> '))
+# if savat:  
+#  for sav in savat:
+#   if sav.title() in mahsulotlar:
+#    print("Menyuda", sav, 'bor.' )
+#   else:
+#    print("Menyuda", sav, "yoq" )
+# else:
+#  print("Savatingiz bosh")
+
+# mahsulotlar = ['Sabzi', "Yog'", "Shkalad", "Apilsen", 'Olma',
+#                "Tarvuz","Choy", "Piyoz", "Bodring", "Qalampir"] 
+# savat =[]
+# print("5 ta mahsulot kiriting")
+# for n in range(5):
+#     savat.append(input(f'{n+1} chi  masulotingiz >>> '))
+    
+# bor_mahsulotlar = []    
+# mavjud_emas = [] 
+# for mahsulot in savat:
+#   if mahsulot in mahsulotlar:
+#     bor_mahsulotlar.append(mahsulot)
+#   else:
+#     mavjud_emas.append(mahsulot)
+
+# if bor_mahsulotlar in mahsulotlar:
+#   print("Siz so'ragan barcha mahsulot do'konimizda bor.")
+# else:
+#  print("Do'konimizda quydagi mahsulot yoq.")
+#  for mahsulot in mavjud_emas: 
+#   print(mahsulot)
+
+# foydalanuvchilar = ['aziz', "anvar", 'sardor', 'begzod', 'jasur']
+# logen = input("Yangi logen tanlang >>> ")
+# if logen in foydalanuvchilar:
+#   print("Logen band yangi logen kiriting", logen.title)
+# else: 
+#   print("Xush kelibsiz !")
+
+# son = int(input("Biror buton son kiriting. "))
+# for n in range(1,11):
+#   if not son%n:
+#    print(f"{son} soni {n} ga qoldiqsiz bo'linadi")
+
+#    GEMINI BERGAN SAVOLLAR.
+# harorat = int(input("Bugun harorat nechi >>>  "))
+# if harorat>30:
+#   print("Bugun juda issiq, muzqaymoq yeng!")
+# elif harorat >15<30:
+#   print("Ob-havo ajoyib, sayirga chiqing")
+# elif harorat<=15:
+#   print("Kun salqin, qalin kiyining")
+
+# for n in range(1,21):
+#  if n%2!=0:
+#   print(n)
+
+# sonlar = [5,-3,0,12,-7,0,10]
+# for son in sonlar:
+#  if son>0:
+#   print(son, "Musbat son")
+#  elif son<0:
+#   print(son, "Manfiy son")
+#  else:
+#   print("Nol")
+
+# parol="Python2024"
+# parol_yoz = input("Parol kiriting >>>  ")
+# if parol in parol_yoz:
+#   print("Xush kelibsiz!")
+# else:
+#   print("Parol noto'g'ri qaytadan urining!")
+
+# mevalar = ['olma', 'banan', 'olcha', 'shaftoli', 'uzum']
+# for meva in mevalar:
+#  if meva.startswith('o'):
+#   print(meva)
+
+# parol = input("Yangi parol kiriting >>> ")
+# if parol == '12345678':
+#     print("Bu juda oddiy parol.")
+# elif len(parol)>=8:
+#     print("Parol muvaffaqiyatli o'rnatildi.") 
+# else:
+#     print("Parol juda qisqa.")
+
+# son = int(input('Biror son kiriting! '))
+# for n in range(1,11):
+#     print(f"{son}*{n}={son*n}")
+
+# mevalar = ['olma', 'banan', 'shaftoli', 'uzum', 'kivi']
+# for meva in mevalar:
+#     if meva=="olma":
+#         print(meva.upper())
+#     else:    
+#         print(meva.title())
+
+# toq_sonlar = []
+# for n in range(1,21,2):
+#     print(f"{n**2}")
+#     toq_sonlar.append(n**2)
+# print(sum(toq_sonlar))    
+
+# yosh = int(input("Yoshingiz nechida: "))
+# if yosh<7:
+#     print("Sizga kirish bepul.")
+# elif 7<=yosh<=18:
+#     print("CHipta narhi 10000 so'm.")
+# elif yosh>65:
+#     print("Sizga 50 % chegirma bor.")
+# else:
+#     print("CHipta narhi 20000 so'm.")
+
+# menyu = ["osh",'shashlik', 'somsa', 'manti', "sho'rva", "lag'mon", "qozon kabob"]
+# buyurtmalar = []
+# print("3 ta taom buyurtma qiling.")
+# for n in range(3):
+#     buyurtma = input(f"{n+1} chi taom. ")
+#     buyurtmalar.append(buyurtma)
+# for buyurtma in buyurtmalar:
+#     if buyurtma in menyu:
+#         print(f"\nBizda {buyurtma} bor tayyorlanmoqda.") 
+#     else:   
+#        print(f"\nBizda {buyurtma} yoq.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
